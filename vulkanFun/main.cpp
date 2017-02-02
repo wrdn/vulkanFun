b06b225eca8813883c8987e4344e573ce063dc2a
William Dann <William Dann>
2017-02-02 21:20:55 +0000

    m

---
+++ vulkanFun/main.cpp
@@ -0,0 +1,270 @@
+#include <vulkan/vulkan.hpp>
+#include <Windows.h>
+#include <GLFW/glfw3.h>
+
+#include <set>
+
+#define TRACE(v, ...) { char buff[512]; sprintf_s(buff, sizeof(buff), "[TRACE] " v "\n", __VA_ARGS__); printf(buff); OutputDebugString(buff); }
+
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugReportFlagsEXT flags,
+    VkDebugReportObjectTypeEXT objType,
+    uint64_t obj,
+    size_t location,
+    int32_t code,
+    const char* layerPrefix,
+    const char* msg,
+    void* userData) {
+
+    TRACE("validation layer %s: %s", layerPrefix, msg);
+    return VK_FALSE;
+};
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "vkTest", nullptr, nullptr);
+
+    // print available instance extensions and layers
+    auto allInstExtensions = vk::enumerateInstanceExtensionProperties();
+    auto allInstLayers = vk::enumerateInstanceLayerProperties();
+
+    TRACE("Available Instance Extensions:");
+    for (auto it : allInstExtensions)
+        TRACE("> %s", it.extensionName);
+
+    TRACE("Available Instance Layers:");
+    for (auto it : allInstLayers)
+        TRACE("> %s", it.layerName);
+
+    bool ADD_VALIDATION_LAYERS = true;
+
+    // Find and add validation layer
+    std::vector<const char*> instLayers;
+    std::vector<const char*> validationLayers;
+
+    if (ADD_VALIDATION_LAYERS)
+    {
+        auto validationLayer = std::find_if(allInstLayers.begin(), allInstLayers.end(), [](const vk::LayerProperties& e) {
+            return strcmp(e.layerName, "VK_LAYER_LUNARG_standard_validation") == 0;
+        });
+        if (validationLayer != allInstLayers.end())
+        {
+            instLayers.push_back(validationLayer->layerName);
+            validationLayers.push_back(validationLayer->layerName);
+        }
+    }
+
+    // Populate required extensions vector then find and add debug report extension
+    uint32_t reqExtCount = 0;
+    const char** reqExt = glfwGetRequiredInstanceExtensions(&reqExtCount);
+    std::vector<const char*> instExtensions;
+    for (uint32_t i = 0; i < reqExtCount; ++i)
+        instExtensions.push_back(reqExt[i]);
+
+    // find debug report extension
+    if (ADD_VALIDATION_LAYERS)
+    {
+        auto debugReportExtension = std::find_if(allInstExtensions.begin(), allInstExtensions.end(), [](const vk::ExtensionProperties& e) {
+            return strcmp(e.extensionName, VK_EXT_DEBUG_REPORT_EXTENSION_NAME) == 0;
+        });
+        if (debugReportExtension != allInstExtensions.end())
+            instExtensions.push_back(debugReportExtension->extensionName);
+    }
+
+    // setup instance creation info
+    vk::ApplicationInfo appInfo("vkTest", 1, "vkTest", 1, VK_API_VERSION_1_0);
+    vk::InstanceCreateInfo instCreateInfo(vk::InstanceCreateFlags(), &appInfo);
+    instCreateInfo.enabledExtensionCount = (uint32_t)instExtensions.size();
+    instCreateInfo.ppEnabledExtensionNames = instExtensions.data();
+    if (instLayers.empty() == false) {
+        instCreateInfo.ppEnabledLayerNames = instLayers.data();
+        instCreateInfo.enabledLayerCount = (uint32_t)instLayers.size();
+    }
+
+    // create instance
+    vk::Instance inst = vk::createInstance(instCreateInfo);
+
+    // setup debug callback
+    vk::DebugReportCallbackCreateInfoEXT debugReportCallbackInfo;
+    debugReportCallbackInfo.flags =
+        vk::DebugReportFlagBitsEXT::eInformation |
+        vk::DebugReportFlagBitsEXT::eWarning |
+        vk::DebugReportFlagBitsEXT::ePerformanceWarning |
+        vk::DebugReportFlagBitsEXT::eError |
+        vk::DebugReportFlagBitsEXT::eDebug;
+    debugReportCallbackInfo.pfnCallback = debugCallback;
+    VkDebugReportCallbackEXT callback;
+    auto createDebugReportFunc = (PFN_vkCreateDebugReportCallbackEXT)inst.getProcAddr("vkCreateDebugReportCallbackEXT");
+    auto debugReportCallbackRes = createDebugReportFunc(VkInstance(inst), &VkDebugReportCallbackCreateInfoEXT(debugReportCallbackInfo), nullptr, &callback);
+
+    // create window surface
+    vk::SurfaceKHR surface;
+    if (glfwCreateWindowSurface(VkInstance(inst), window, nullptr, (VkSurfaceKHR*)&surface) != VK_SUCCESS)
+        return 0;
+
+    // enumerate devices, print details and select best one (for now, just select the first entry)
+    auto allPhysDevices = inst.enumeratePhysicalDevices();
+    if (allPhysDevices.empty())
+        return 0;
+    for (auto it : allPhysDevices)
+    {
+        auto props = it.getProperties();
+        TRACE("Device: %s", props.deviceName);
+    }
+    auto physDevice = allPhysDevices[0];
+
+    // find graphics + present queues
+    auto queueFamilies = physDevice.getQueueFamilyProperties();
+    int graphics_queue_ix = -1;
+    int present_queue_ix = -1;
+
+    for (int i = 0; i < (int)queueFamilies.size(); ++i)
+    {
+        auto presentSupport = physDevice.getSurfaceSupportKHR(i, surface);
+        if (queueFamilies[i].queueFlags & vk::QueueFlagBits::eGraphics)
+        {
+            if (graphics_queue_ix == -1)
+                graphics_queue_ix = i;
+
+            if (presentSupport)
+            {
+                graphics_queue_ix = i;
+                present_queue_ix = i;
+                break;
+            }
+        }
+        else if (present_queue_ix == -1 && presentSupport)
+        {
+            present_queue_ix = i;
+        }
+    }
+
+    if (graphics_queue_ix == -1 || present_queue_ix == -1)
+        return 0;
+
+    TRACE("Found gfx (%d) and present (%d) queues!", graphics_queue_ix, present_queue_ix);
+
+    // print extensions and layers supported by selected device
+    auto allPhysDeviceExtensions = physDevice.enumerateDeviceExtensionProperties();
+    auto allPhysDeviceLayers = physDevice.enumerateDeviceLayerProperties();
+
+    TRACE("Physical Device '%s' supported Extensions:", physDevice.getProperties().deviceName);
+    for (auto it : allPhysDeviceExtensions)
+        TRACE("> %s", it.extensionName);
+
+    TRACE("Physical Device '%s' supported Layers:", physDevice.getProperties().deviceName);
+    for (auto it : allPhysDeviceLayers)
+        TRACE("> %s", it.layerName);
+
+    // make sure VK_KHR_SWAPCHAIN_EXTENSION_NAME is supported
+    auto swapchainExt = std::find_if(allPhysDeviceExtensions.begin(), allPhysDeviceExtensions.end(), [](vk::ExtensionProperties& e) {
+        return strcmp(e.extensionName, VK_KHR_SWAPCHAIN_EXTENSION_NAME) == 0;
+    });
+    if (swapchainExt == allPhysDeviceExtensions.end())
+        return 0;
+
+
+
+    // create the logical device
+    vk::PhysicalDeviceFeatures physDeviceFeatures;
+
+    // setup queue info for graphics + presentation queues,
+    // which _might_ be different
+    std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos;
+    std::set<int> uniqueQueueFamilies = { graphics_queue_ix, present_queue_ix };
+    float qPriority = 1.0f;
+    for (auto it : uniqueQueueFamilies)
+    {
+        vk::DeviceQueueCreateInfo queueCreateInfo;
+        queueCreateInfo.queueCount = 1;
+        queueCreateInfo.queueFamilyIndex = it;
+        queueCreateInfo.pQueuePriorities = &qPriority;
+
+        queueCreateInfos.push_back(queueCreateInfo);
+    }
+
+    vk::DeviceCreateInfo deviceCreateInfo;
+    deviceCreateInfo.pQueueCreateInfos = queueCreateInfos.data();
+    deviceCreateInfo.queueCreateInfoCount = (uint32_t)queueCreateInfos.size();
+    deviceCreateInfo.pEnabledFeatures = &physDeviceFeatures;
+    
+    // enable the swapchain extension (searched for above)
+    const char* swapchainExtId[] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+    deviceCreateInfo.enabledExtensionCount = 1;
+    deviceCreateInfo.ppEnabledExtensionNames = swapchainExtId;
+
+    // and validation layers
+    if (ADD_VALIDATION_LAYERS)
+    {
+        deviceCreateInfo.enabledLayerCount = (uint32_t)validationLayers.size();
+        deviceCreateInfo.ppEnabledLayerNames = validationLayers.data();
+    }
+
+    // create the logical device now!
+    auto dev = physDevice.createDevice(deviceCreateInfo);
+
+    auto gfxQueue = dev.getQueue(graphics_queue_ix, 0);
+    auto presentQueue = graphics_queue_ix == present_queue_ix ?
+        gfxQueue :
+        dev.getQueue(present_queue_ix, 0);
+
+    //auto surfaceCaps = physDevice.getSurfaceCapabilitiesKHR(surface);
+    auto surfaceFormats = physDevice.getSurfaceFormatsKHR(surface);
+    auto surfacePresentModes = physDevice.getSurfacePresentModesKHR(surface);
+
+    if (surfaceFormats.empty() || surfacePresentModes.empty())
+        return 0;
+
+
+    // get best surface format for swapchain
+    vk::SurfaceFormatKHR selectedSurfaceFormat = surfaceFormats[0];
+
+    if (surfaceFormats.size() == 1 &&
+        selectedSurfaceFormat.format == vk::Format::eUndefined)
+    {
+        // surface has no preferred format, yay :)
+        selectedSurfaceFormat.format = vk::Format::eB8G8R8A8Unorm;
+        selectedSurfaceFormat.colorSpace = vk::ColorSpaceKHR::eSrgbNonlinear;
+    }
+    else
+    {
+        auto f = std::find_if(surfaceFormats.begin(), surfaceFormats.end(), [](const vk::SurfaceFormatKHR& fm) {
+            return fm.format == vk::Format::eB8G8R8A8Unorm && fm.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear;
+        });
+        if (f != surfaceFormats.end())
+            selectedSurfaceFormat = *f;
+    }
+
+    // get best present mode for swapchain
+    vk::PresentModeKHR selectedPresentMode = vk::PresentModeKHR::eFifo;
+    for (auto it : surfacePresentModes)
+    {
+        if (it == vk::PresentModeKHR::eMailbox)
+        {
+            selectedPresentMode = it;
+            break;
+        }
+    }
+
+    while (!glfwWindowShouldClose(window))
+        glfwPollEvents();
+
+    // shutdown
+    dev.waitIdle();
+    inst.destroySurfaceKHR(surface);
+    glfwDestroyWindow(window);
+
+    auto destroyDebugReportFunc = (PFN_vkDestroyDebugReportCallbackEXT)inst.getProcAddr("vkDestroyDebugReportCallbackEXT");
+    destroyDebugReportFunc(VkInstance(inst), callback, nullptr);
+
+    dev.destroy();
+    inst.destroy();
+
+    return 0;
+}
