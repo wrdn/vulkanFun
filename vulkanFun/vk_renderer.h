dece16868593adcd6376da6afdefc8d7b1da1481
William Dann <williamdann@hotmail.com>
2017-02-05 00:05:52 +0000

    split vk code into created vk_renderer* files

---
+++ vulkanFun/vk_renderer.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+
+static const unsigned WIDTH = 640;
+static const unsigned HEIGHT = 480;
+
+struct GLFWwindow;
+
+class VKRenderer
+{
+public:
+    void                          createInstance();
+    void                          setupDebugCallback();
+    void                          createSurface(GLFWwindow* window);
+    void                          selectPhysicalDevice();
+    void                          selectLogicalDevice();
+    void                          createSwapChain();
+    void                          createRenderPass();
+    void                          loadShaders();
+    void                          createPipelineCache();
+    void                          flushPipelineCache();
+    void                          createGraphicsPipeline();
+
+    void                          shutdown();
+
+private:
+    vk::Instance                  m_inst;
+
+    vk::SurfaceKHR                m_surface;
+    vk::PhysicalDevice            m_physDevice;
+    vk::Device                    m_dev;
+
+    int                           m_gfxQueueIx;
+    int                           m_presentQueueIx;
+
+    vk::SwapchainKHR              m_swapChain;
+    vk::Format                    m_swapChainImageFormat;
+    vk::Extent2D                  m_swapExtent;
+    std::vector<vk::Image>        m_swapChainImages;
+    std::vector<vk::ImageView>    m_swapChainImageViews;
+
+    vk::RenderPass                m_renderPass;
+
+    vk::Pipeline                  m_gfxPipeline;
+    vk::PipelineLayout            m_gfxPipelineLayout;
+    vk::PipelineCache             m_pipelineCache;
+
+    // test shaders
+    vk::ShaderModule              m_vertShader;
+    vk::ShaderModule              m_fragShader;
+
+    VkDebugReportCallbackEXT      m_debugCallback;
+    std::vector<const char*>      m_validationLayers;
+};
