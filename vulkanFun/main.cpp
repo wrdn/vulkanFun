c1637325b7bb57c691c8c1b9628fb098a1053295
William Dann <williamdann@hotmail.com>
2017-04-09 15:59:14 +0100

    print video modes using glfw

--- vulkanFun/main.cpp
+++ vulkanFun/main.cpp
@@ -1,3 +1,4 @@
+#include "trace.h"
 #include <GLFW/glfw3.h>
 #include "vk_renderer.h"
 
@@ -13,12 +14,30 @@ static void onWindowResized(GLFWwindow* window, int width, int height)
     r.recreateSwapChain((uint32_t)width, (uint32_t)height);
 }
 
+static void printVideoModes()
+{
+    auto* primaryMonitor = glfwGetPrimaryMonitor();
+
+    int c = 0;
+    auto* videoModes = glfwGetVideoModes(primaryMonitor, &c);
+    for (int i = 0; i < c;++i)
+    {
+        auto v = videoModes[i];
+        TRACE("%d %d @%dhz", v.width, v.height, v.refreshRate);
+    }
+
+    auto* currentVideoMode = glfwGetVideoMode(primaryMonitor);
+    TRACE("current video mode: %d %d @%dhz", currentVideoMode->width, currentVideoMode->height, currentVideoMode->refreshRate);
+}
+
 int main()
 {
     const unsigned WIDTH = 640;
     const unsigned HEIGHT = 480;
-
+    
     glfwInit();
+
+    printVideoModes();
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "vkTest", nullptr, nullptr);
